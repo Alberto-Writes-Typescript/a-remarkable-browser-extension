e2d2e1fca60cf15f4ba4b62d28bce92f99324f9d
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-09 09:55:25 +0200

    fix: linter issues

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,20 +1,20 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 import '../assets/css/style.css'
 
-function IndexPopup() {
-  const [data, setData] = useState("")
+function IndexPopup (): JSX.Element {
+  const [data, setData] = useState('')
 
   return (
     <div className="bg-black">
       <h2>
-        Welcome to your{" "}
+        Welcome to your{' '}
         <a href="https://www.plasmo.com" target="_blank">
           Plasmo
-        </a>{" "}
+        </a>{' '}
         Extension!
       </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
+      <input onChange={(e) => { setData(e.target.value) }} value={data} />
       <a href="https://docs.plasmo.com" target="_blank">
         View Docs
       </a>

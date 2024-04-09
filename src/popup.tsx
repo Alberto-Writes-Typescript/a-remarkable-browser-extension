cc59c4b708e31c94f0449bb1fa64d05ccd4819fb
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-09 09:07:43 +0200

    chore: install TailwindCSS
    Install TailwindCSS dependencies. Add Tailwind configuration to project and import style into pop-up page.

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,13 +1,12 @@
 import { useState } from "react"
 
+import './style.css'
+
 function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
+    <div className="bg-black">
       <h2>
         Welcome to your{" "}
         <a href="https://www.plasmo.com" target="_blank">

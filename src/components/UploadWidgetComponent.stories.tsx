63edaa3454fce7dc496a6e39c913a4605b3e2627
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-10 10:34:53 +0200

    feat: Basic layout for upload button.

---
+++ src/components/UploadWidgetComponent.stories.tsx
@@ -0,0 +1,8 @@
+import UploadWidgetComponent from './UploadWidgetComponent'
+
+export default {
+  title: 'UploadWidgetComponent',
+  component: UploadWidgetComponent
+}
+
+export const Default = {}

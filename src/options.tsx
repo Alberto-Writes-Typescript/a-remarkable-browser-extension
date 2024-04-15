4171978d8f7a7af27bee8160674de44b0c2ccf03
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-15 11:37:10 +0200

    feat: Pairing wizard.
    Add a pairing wizard component that will provide an UI to connect the extension to the reMarkable cloud.

--- src/options.tsx
+++ src/options.tsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 import '../assets/css/style.css'
+import PairingWizard from './options/pairingWizard'
 
 function Options (): React.ReactElement {
+  const [deviceToken, setDeviceToken] = useState(null)
+
+  function paired (): boolean {
+    return deviceToken !== null
+  }
+
+  function deviceTokenManager (): React.ReactElement {
+    return <p>Paired</p>
+  }
+
   return (
-    <div className="h-screen w-screen flex items-center justify-center p-16 bg-gray-50">
+    <div className="h-screen w-screen flex items-center justify-center p-16 bg-gray-50 text-gray-400 text-xs">
       <div className="h-fit min-h-[650px] w-[450px] flex flex-row border border-gray-400 rounded">
-        <div className="w-10 bg-gray-400 relative">
-          <p className="absolute bottom-6 left-1 text-xs text-white -rotate-90">v0.1.0</p>
+        <div className="w-20 bg-gray-400 relative">
+          <p className="absolute bottom-6 text-xs text-white -rotate-90">v0.1.0</p>
         </div>
-        <div className="flex-grow p-8 space-y-2">
-          <h1 className="text-xl text-gray-500">Configuration</h1>
+        <div className="grow p-8 space-y-4">
+          <h1 className="text-xl">Configuration</h1>
+
+          { paired() ? deviceTokenManager() : PairingWizard() }
         </div>
       </div>
     </div>

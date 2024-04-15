3f94bee5ccf2b1c69a86a230ffe6c2376adfcbde
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-15 16:25:25 +0200

    feat: Token storage with `ConnectionManager`.
    `ConnectionManager` is now updated to persist tokens in a secured storage. The idea is to persist the device and session tokens so they are available whenever the user opens the web browser.
    
    I still need to see how safe it is persisting the tokens in there, but at the moment this is a great approach to define the connection logic in isolation and integrate it easily in the service worker logic.

--- src/lib/ConnectionManager.ts
+++ src/lib/ConnectionManager.ts
@@ -1,25 +1,60 @@
 import { RemarkableClient, type Device, type Session } from 'a-remarkable-js-sdk'
+import { SecureStorage } from '@plasmohq/storage/secure'
 import { v4 as uuidv4 } from 'uuid'
 
 export default class ConnectionManager {
-  readonly #reMarkableClient: RemarkableClient
+  #reMarkableClient: RemarkableClient
+
+  readonly #tokenStore: SecureStorage
 
   constructor () {
+    this.#tokenStore = new SecureStorage()
     this.#reMarkableClient = RemarkableClient.withFetchHttpClient()
   }
 
+  async deviceToken (): Promise<string | undefined> {
+    return this.#tokenStore.get('deviceToken')
+  }
+
+  async sessionToken (): Promise<string | undefined> {
+    return this.#tokenStore.get('sessionToken')
+  }
+
   async pair (oneTimeCode: string): Promise<Device> {
+    const deviceToken = await this.deviceToken()
+
+    if (deviceToken != null) {
+      this.#reMarkableClient = RemarkableClient.withFetchHttpClient(deviceToken)
+
+      return this.#reMarkableClient.device
+    }
+
     if (!this.#reMarkableClient.paired) {
       await this.#reMarkableClient.pair(uuidv4(), 'browser-chrome', oneTimeCode)
     }
+
+    this.#tokenStore.set('deviceToken', this.#reMarkableClient.device.token)
 
     return this.#reMarkableClient.device
   }
 
   async connect (): Promise<Session> {
-    if (this.#reMarkableClient.sessionExpired) {
-      await this.#reMarkableClient.connect()
+    const deviceToken = await this.deviceToken()
+    const sessionToken = await this.sessionToken()
+
+    this.#reMarkableClient = RemarkableClient.withFetchHttpClient(deviceToken, sessionToken)
+
+    if (!this.#reMarkableClient.paired) {
+      throw new Error('Device not paired')
     }
+
+    if (!this.#reMarkableClient.sessionExpired) {
+      return this.#reMarkableClient.session
+    }
+
+    await this.#reMarkableClient.connect()
+
+    this.#tokenStore.set('sessionToken', this.#reMarkableClient.session.token)
 
     return this.#reMarkableClient.session
   }

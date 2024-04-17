91433d5c92c3e5af84be65b14f0531cc7a414823
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-17 15:27:07 +0200

    feat: DeviceConnector component logic.
    Added logic to pair extension with reMarkable Cloud through Options component.

--- src/options.tsx
+++ src/options.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import { type Device } from 'a-remarkable-js-sdk'
+import React, { useCallback, useState } from 'react'
+import { Device } from 'a-remarkable-js-sdk'
+import ConnectionManager from './lib/services/remarkable/ConnectionManager'
 import DeviceConnector from './components/options/deviceConnector'
 import Layout from './components/options/layout'
 import Settings from './components/options/settings'
@@ -9,10 +10,26 @@ import '../assets/css/style.css'
 function Options (): React.ReactElement {
   const [device, setDevice] = useState<Device | null>(null)
 
+  const pairDevice = useCallback(async (oneTimeCode: string): Promise<Device | null> => {
+    const connectionManager = new ConnectionManager()
+
+    try {
+      const deviceToken = await connectionManager.pair(oneTimeCode)
+
+      const device = new Device(deviceToken)
+
+      setDevice(device)
+
+      return device
+    } catch (error) {
+      return null
+    }
+  }, [])
+
   return (
     <Layout>
       <div className="h-full w-full p-8 space-y-4">
-        { device != null ? <Settings/> : <DeviceConnector/> }
+        { device != null ? <Settings/> : <DeviceConnector pairDevice={pairDevice}/> }
       </div>
     </Layout>
   )

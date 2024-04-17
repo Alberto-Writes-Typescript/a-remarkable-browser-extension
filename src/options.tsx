abb086da4629b45fd86671138af77e05e1d702cc
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-17 14:30:05 +0200

    feat: DeviceConnector layout.
    Screen to pair extension with reMarkable device.

--- src/options.tsx
+++ src/options.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
+import { type Device } from 'a-remarkable-js-sdk'
+import DeviceConnector from './components/options/deviceConnector'
 import Layout from './components/options/layout'
 import Settings from './components/options/settings'
 
 import '../assets/css/style.css'
 
 function Options (): React.ReactElement {
+  const [device, setDevice] = useState<Device | null>(null)
+
   return (
     <Layout>
-      <div className="p-8 space-y-4">
-        <Settings/>
+      <div className="h-full w-full p-8 space-y-4">
+        { device != null ? <Settings/> : <DeviceConnector/> }
       </div>
     </Layout>
   )

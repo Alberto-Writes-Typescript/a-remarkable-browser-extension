74f17d36660776dd5069186a290392e15042ad1c
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-17 12:47:26 +0200

    refactor: Optional props for Heading component.
    Allow Heading component to accept any arbitrary set of props. These props will be passed to the component root element.

--- src/components/common/heading.tsx
+++ src/components/common/heading.tsx
@@ -1,13 +1,23 @@
 interface HeadingProps {
   as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
   children?: React.ReactNode
+  [x: string]: unknown
+}
+
+const HEADING_STYLES = {
+  h1: '!text-xl',
+  h2: '!text-base',
+  h3: '!text-sm'
 }
 
 export default function Heading ({ as, children, ...rest }: HeadingProps): React.ReactElement {
   const Component = as ?? 'h1'
 
   return (
-    <Component className="text-xl font-semibold tracking-wide" {...rest}>
+    <Component
+      className={ `text-xl font-semibold tracking-wide ${HEADING_STYLES[Component]}` }
+      {...rest}
+    >
       {children}
     </Component>
   )

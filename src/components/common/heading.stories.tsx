dd2f65c9d608b305638131c61c3a22298e7646d7
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-17 12:28:44 +0200

    feat: Heading Component

---
+++ src/components/common/heading.stories.tsx
@@ -0,0 +1,13 @@
+import heading from './heading'
+
+export default {
+  title: 'Common/Heading',
+  component: heading
+}
+
+export const Default = {
+  args: {
+    as: 'h1',
+    children: 'Heading Component'
+  }
+}

0ed48c89f1deaf5bbfd591a230ad71995cc50c4e
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-18 09:10:51 +0200

    feat: Logic to pair device and display information ready.
    There is still a lot of work to do in terms of maintenance, but I would rather keep exploring how the whole Plasmo-React architecture work and then set up some proper testing.

--- src/components/options/settings.tsx
+++ src/components/options/settings.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import Button from '../common/button'
 import Heading from '../common/heading'
+import type { Device } from 'a-remarkable-js-sdk'
 
-function Settings (): React.ReactElement {
+interface SettingsProps {
+  device: Device
+  unpair: () => Promise<void>
+}
+
+function Settings ({ device, unpair }: SettingsProps): React.ReactElement {
   return (
     <div className="w-full h-full space-y-8 text-gray-600">
       <Heading>Configuration</Heading>
@@ -11,14 +17,13 @@ function Settings (): React.ReactElement {
         <Heading as="h3">reMarkable Cloud Connection</Heading>
 
         <div className="border border-gray-500 p-3 space-y-2 text-sm">
-          <p className="font-thin">Device <span className="!font-normal float-right">44123-21312-31231-123321</span></p>
+          <p className="font-thin">Device <span className="!font-normal float-right">{device.id}</span></p>
           <hr/>
-          <p className="font-thin">Connected at <span className="!font-normal float-right">12/12/2024</span></p>
+          <p className="font-thin">Connected at <span className="!font-normal float-right">12.12.2024</span></p>
         </div>
 
         <div className="w-full inline-flex gap-4 justify-center">
-          <Button>Reconnect</Button>
-          <Button>Disconnect</Button>
+          <Button onClick={unpair}>Disconnect</Button>
         </div>
       </div>
     </div>

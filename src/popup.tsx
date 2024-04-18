c1e67e30aaa64e6d30fc7159b43a45a02b62b39c
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-18 10:56:30 +0200

    feat: popup fetches `Device` from configuration and displays either upload dashboard or welcome screen depending on the extension being already paired or not.

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Dashboard from './components/popup/dashboard'
 import Welcome from './components/popup/welcome'
 
 import '../assets/css/style.css'
+import ConfigurationManager from './lib/services/ConfigurationManager'
+import { Device } from 'a-remarkable-js-sdk'
 
-function IndexPopup (): JSX.Element {
+function IndexPopup (): React.ReactElement {
+  const [device, setDevice] = useState<Device | null>(null)
+
+  useEffect(() => {
+    async function fetchDeviceFromConfiguration (): Promise<void> {
+      const configurationManager = new ConfigurationManager()
+      const deviceToken = await configurationManager.deviceToken()
+
+      if (deviceToken != null) setDevice(new Device(deviceToken))
+    }
+
+    void fetchDeviceFromConfiguration().then(r => {})
+  }, [])
+
   return (
     <div className="min-w-[500px] mx-4 my-4 px-8 py-6 bg-gray-50 border border-gray-400">
-      <Welcome/>
+      { device != null ? <Dashboard device={device}/> : <Welcome/> }
     </div>
   )
 }

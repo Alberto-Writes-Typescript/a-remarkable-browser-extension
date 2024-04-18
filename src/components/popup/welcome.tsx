0f70e92c7c3b76807dafaa8b63c85a3ab04048d2
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-18 10:53:09 +0200

    feat: popup welcome component.
    Basic component displayed when extension has no device attached. Redirects to options page to start with the pairing process.

---
+++ src/components/popup/welcome.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Button from '../common/button'
+import Heading from '../common/heading'
+
+function Welcome (): React.ReactElement {
+  return (
+    <div className="h-full w-[90%] flex flex-col items-center justify-center gap-6 m-auto">
+      <Heading>No device connected</Heading>
+      <p className="text-sm font-normal text-gray-600 text-center">
+        Start uploading documents to your reMarkable tablet by pairing
+        your device with this extension
+      </p>
+
+      <Button as="a" href={`chrome-extension://${chrome?.runtime?.id}/options.html`} target="_blank">
+        open extension settings
+      </Button>
+    </div>
+  )
+}
+
+export default Welcome

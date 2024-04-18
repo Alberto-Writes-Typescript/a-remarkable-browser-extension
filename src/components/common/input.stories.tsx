385a66b08f3a3243646f0fa14fcab51879cba02f
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-18 10:22:50 +0200

    chore: add stories for `Input` component.

--- src/components/common/input.stories.tsx
+++ src/components/common/input.stories.tsx
@@ -1,8 +1,54 @@
-import input from './input'
+import Input, { type InputProps } from './input'
+import { type Meta, type StoryFn } from '@storybook/react'
 
 export default {
   title: 'Common/Input',
-  component: input
+  component: Input,
+  argTypes: {
+    size: {
+      control: 'radio',
+      options: ['small', 'medium', 'large']
+    },
+    variant: {
+      control: 'radio',
+      options: ['primary', 'oneTimeCode']
+    },
+    placeholder: {
+      control: 'text'
+    }
+  }
+} satisfies Meta
+
+const Template: StoryFn<InputProps> = (args) => <Input {...args} />
+
+export const Playground = Template.bind({})
+
+export const Small = Template.bind({})
+Small.args = { size: 'small' }
+Small.argTypes = {
+  size: { table: { disable: true } }
 }
 
-export const Default = {}
+export const Medium = Template.bind({})
+Medium.args = { size: 'medium' }
+Medium.argTypes = {
+  size: { table: { disable: true } }
+}
+
+export const Large = Template.bind({})
+Large.args = { size: 'large' }
+Large.argTypes = {
+  size: { table: { disable: true } }
+}
+
+export const Primary = Template.bind({})
+Primary.args = { variant: 'primary' }
+Primary.argTypes = {
+  variant: { table: { disable: true } }
+}
+
+export const OneTimeCode = Template.bind({})
+OneTimeCode.args = { variant: 'oneTimeCode' }
+OneTimeCode.argTypes = {
+  variant: { table: { disable: true } }
+}
--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -1,13 +1,37 @@
 import React from 'react'
 
-type InputProps = Record<string, unknown>
+export interface InputProps {
+  size?: 'small' | 'medium' | 'large'
+  variant?: 'primary' | 'oneTimeCode'
+  [x: string]: unknown
+}
 
-export default function Button ({ ...rest }: InputProps): React.ReactElement {
-  const className = `
-    p-2 bg-transparent border border-gray-600 text-sm text-gray-600 text-center
-    transition-all
-    focus:border-gray-700 focus:bg-gray-100 focus:outline-none focus:ring-0
+const INPUT_SIZES = {
+  small: 'text-sm',
+  medium: 'py-2 text-sm',
+  large: 'py-3 text-base'
+}
+
+const INPUT_VARIANTS = {
+  primary: `
+    border-b border-gray-400 text-gray-600
+    focus:border-gray-700
+    hover:border-gray-700
+  `,
+  oneTimeCode: `
+    border border-gray-400 text-gray-600 text-center
+    focus:border-gray-700 focus:bg-gray-100
     hover:border-gray-700 hover:bg-gray-100
+    disabled:text-gray-400 disabled:bg-gray-100 disabled:border-gray-400
+  `
+}
+
+export default function Button ({ size, variant, ...rest }: InputProps): React.ReactElement {
+  const className = `
+    transition-all bg-transparent
+    focus:outline-none focus:ring-0
+    ${INPUT_SIZES[size ?? 'medium']}
+    ${INPUT_VARIANTS[variant ?? 'primary']}
   `
 
   return (

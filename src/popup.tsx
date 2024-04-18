6229ca3f2e7f5b1723c7b93af213fba54fe825fd
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-18 10:27:36 +0200

    feat: layout for pop-up document upload ready.

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,11 +1,12 @@
-import DashboardComponent from '../src/components/debugger/DashboardComponent'
+import React from 'react'
+import Dashboard from './components/popup/dashboard'
 
 import '../assets/css/style.css'
 
 function IndexPopup (): JSX.Element {
   return (
-    <div className="bg-transparent w-fit min-w-[500px]">
-      <DashboardComponent/>
+    <div className="px-8 py-6 min-w-[500px]">
+      <Dashboard device={null}/>
     </div>
   )
 }

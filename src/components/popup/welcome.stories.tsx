03872d6997a564205cb335be83ec3039937e443e
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-18 10:53:09 +0200

    feat: popup welcome component.
    Basic component displayed when extension has no device attached. Redirects to options page to start with the pairing process.

---
+++ src/components/popup/welcome.stories.tsx
@@ -0,0 +1,8 @@
+import welcome from './welcome'
+
+export default {
+  title: 'Popup/Welcome',
+  component: welcome
+}
+
+export const Default = {}
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Dashboard from './components/popup/dashboard'
+import Welcome from './components/popup/welcome'
 
 import '../assets/css/style.css'
 
 function IndexPopup (): JSX.Element {
   return (
-    <div className="px-8 py-6 min-w-[500px]">
-      <Dashboard device={null}/>
+    <div className="min-w-[500px] mx-4 my-4 px-8 py-6 bg-gray-50 border border-gray-400">
+      <Welcome/>
     </div>
   )
 }

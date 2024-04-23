86cbeaedbe7be2267bc1429462a26052d126108e
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-23 09:09:27 +0200

    feat: Description List component.
    Adds a new React component which represents a Description List.
    
    A description list is a HTML element that displays pairs of terms-descriptions.

---
+++ src/components/common/descriptionList.stories.tsx
@@ -0,0 +1,35 @@
+
+import { type Meta } from '@storybook/react'
+import descriptionList, { DescriptionListDescription, DescriptionListTerm } from './descriptionList'
+
+export default {
+  title: 'Common/DescriptionList',
+  component: descriptionList
+} satisfies Meta
+
+export const WithTermsViaArguments = {
+  args: {
+    listItems: {
+      'Term 1': 'Description 1',
+      'Term 2': 'Description 2',
+      'Term ...': 'Description ...'
+    }
+  }
+}
+
+export const WithTermsViaChildren = {
+  args: {
+    children: (
+      <>
+        <DescriptionListTerm>Term 1</DescriptionListTerm>
+        <DescriptionListDescription>Description 1</DescriptionListDescription>
+
+        <DescriptionListTerm>Term 2</DescriptionListTerm>
+        <DescriptionListDescription>Description 2</DescriptionListDescription>
+
+        <DescriptionListTerm>Term ...</DescriptionListTerm>
+        <DescriptionListDescription>Description ...</DescriptionListDescription>
+      </>
+    )
+  }
+}

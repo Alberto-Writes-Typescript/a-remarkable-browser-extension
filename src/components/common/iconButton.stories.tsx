778969ec02ac7249ee5b37a46126fde207dde872
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-05-03 17:34:40 +0200

    feat: SVG icons in icon button component.
    Now the SVGs are working and encapsulated in components I can replace the previous placeholder by actual icons.
    
    I updated the IconButton props and stories to select specific icons, and extended the styling logic to properly customize the icon inside it.

--- src/components/common/iconButton.stories.tsx
+++ src/components/common/iconButton.stories.tsx
@@ -1,11 +1,16 @@
 import { type StoryFn } from '@storybook/react'
 
 import IconButton, { ICON_BUTTON_SIZES, ICON_BUTTON_STYLES, type IconButtonProps } from './iconButton'
+import { ICONS_LIBRARY } from './icon'
 
 export default {
   title: 'Common/IconButton',
   component: IconButton,
   argTypes: {
+    icon: {
+      control: 'select',
+      options: Object.keys(ICONS_LIBRARY)
+    },
     as: {
       control: 'select',
       options: ['button', 'a', 'div']
@@ -26,10 +31,10 @@ const Template: StoryFn<IconButtonProps> = (args) => <IconButton {...args} />
 export const Playground = Template.bind({})
 
 export const Small = Template.bind({})
-Small.args = { size: 'small' }
+Small.args = { size: 'sm', icon: 'upload' }
 
 export const Medium = Template.bind({})
-Medium.args = { size: 'medium' }
+Medium.args = { size: 'base', icon: 'upload' }
 
 export const Primary = Template.bind({})
-Primary.args = { theme: 'primary' }
+Primary.args = { theme: 'primary', icon: 'upload' }

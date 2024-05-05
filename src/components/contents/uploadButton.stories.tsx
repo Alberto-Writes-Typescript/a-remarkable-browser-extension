23ef87746139113144581406527b790ca2ca6b0d
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-05-05 17:10:19 +0200

    refactor:  UI refactor of the uploadButton.
    - UI states for the different upload stages.

--- src/components/contents/uploadButton.stories.tsx
+++ src/components/contents/uploadButton.stories.tsx
@@ -10,7 +10,29 @@ export const Default = {
   args: {
     documentPreview: new DocumentPreview('https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', 100),
     uploadDocument: async (fileName: string, url: string) => {
+      const promise = new Promise((resolve, reject) => {
+        setTimeout(() => { resolve(null) }, 3000)
+      })
+
+      await promise
+
       console.log('Uploading document:', fileName, 'from:', url)
+    }
+  }
+}
+
+export const UnsuccesfulUpload = {
+  args: {
+    documentPreview: new DocumentPreview('https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', 100),
+    uploadDocument: async (fileName: string, url: string) => {
+      const promise = new Promise((resolve, reject) => {
+        setTimeout(() => { resolve(null) }, 3000)
+      })
+
+      await promise
+
+      // eslint-disable-next-line @typescript-eslint/no-throw-literal
+      throw ('Failed to upload document')
     }
   }
 }
--- src/components/contents/uploadButton.tsx
+++ src/components/contents/uploadButton.tsx
@@ -11,6 +11,26 @@ import Button from '../common/button'
 import UploadPreview from './uploadPreview'
 import Icon from '../common/icon'
 
+const BUTTON_ICON = {
+  waiting: 'logoAcronym',
+  uploading: 'logoAcronym',
+  uploaded: 'check',
+  failed: 'warning'
+}
+
+const BUTTON_LABEL = {
+  waiting: 'upload to reMarkable',
+  uploading: 'uploading...',
+  uploaded: 'uploaded',
+  failed: 'upload failed, try again'
+}
+
+const BUTTON_LABEL_ICON = {
+  waiting: 'documentUpload',
+  uploading: 'spin',
+  failed: 'documentUpload'
+}
+
 export interface UploadButtonProps {
   documentPreview: DocumentPreview
   uploadDocument: (fileName: string, url: string) => Promise<void>
@@ -19,7 +39,26 @@ export interface UploadButtonProps {
 export default function UploadButton ({ documentPreview, uploadDocument }: UploadButtonProps): React.ReactElement {
   const [fileName, setFileName] = useState<string | undefined>(documentPreview.name)
   const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [uploadStatus, setUploadStatus] = useState<string>('waiting')
 
+  // Upload logic
+  // ------------
+  async function startUpload (): Promise<void> {
+    if (!['waiting', 'failed'].includes(uploadStatus)) return
+
+    setUploadStatus('uploading')
+
+    try {
+      await uploadDocument(fileName as string, documentPreview.url)
+
+      setUploadStatus('uploaded')
+    } catch {
+      setUploadStatus('failed')
+    }
+  }
+
+  // Tooltip Configuration
+  // ---------------------
   const { refs, floatingStyles, context } = useFloating({
     open: isOpen,
     onOpenChange: setIsOpen,
@@ -42,27 +81,27 @@ export default function UploadButton ({ documentPreview, uploadDocument }: Uploa
       <div className='group/upload-button w-fit inline-flex' ref={refs.setReference} {...getReferenceProps()}>
         <IconButton
           className='border-r-0'
-          icon='logoAcronym'
-          onClick={() => uploadDocument(fileName as string, documentPreview.url)}
+          icon={ BUTTON_ICON[uploadStatus] }
+          onClick={async (): Promise<void> => { await startUpload() }}
           size='sm'
         />
         <Button
           className={
             `
               border-l-0 inline-flex items-center justify-center gap-2 text-nowrap
-              animate-all ease-out duration-400 overflow-hidden w-0 px-0
-              group-hover/upload-button:w-[200px] group-hover/upload-button:px-4
+              animate-all ease-out duration-400 overflow-hidden !w-0 !px-0
+              group-hover/upload-button:!w-[200px] group-hover/upload-button:!px-4
             `
           }
-          onClick={() => uploadDocument(fileName as string, documentPreview.url)}
+          onClick={async (): Promise<void> => { await startUpload() }}
           size='sm'
         >
-          upload to reMarkable
-          <Icon icon='documentUpload' size='xs'/>
+          { BUTTON_LABEL[uploadStatus] }
+          <Icon icon={ BUTTON_LABEL_ICON[uploadStatus] } size='xxs' className={ uploadStatus === 'uploading' && 'animate-spin' }/>
         </Button>
       </div>
 
-      {isOpen && (
+      {isOpen && uploadStatus !== 'uploaded' && (
         <div ref={refs.setFloating} style={floatingStyles} aria-labelledby={headingId} {...getFloatingProps()}>
           <UploadPreview fileName={fileName} setFileName={setFileName} documentPreview={documentPreview}/>
         </div>

cf78d241169f0c62886b1f42fd0b9f3472318c24
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-05-08 09:29:31 +0200

    feat: Add leading and trailing icons for heading component.
    I also did some clean up of the code, updating the stories and fixing the load of inlined classes.

--- src/components/common/heading.tsx
+++ src/components/common/heading.tsx
@@ -1,24 +1,43 @@
-interface HeadingProps {
-  as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+import Icon, { type IconName, type IconSize } from './icon'
+
+export type HeadingSize = 'h1' | 'h2' | 'h3'
+
+export const HEADING_STYLES = {
+  h1: '!text-2xl',
+  h2: '!text-xl',
+  h3: '!text-lg'
+}
+
+const ICON_SIZES = {
+  h1: 'sm',
+  h2: 'xs',
+  h3: 'xxs'
+}
+
+export interface HeadingProps {
+  as?: HeadingSize
+  leadingIcon?: IconName
+  trailingIcon?: IconName
   children?: React.ReactNode
   [x: string]: unknown
 }
 
-const HEADING_STYLES = {
-  h1: '!text-xl',
-  h2: '!text-base',
-  h3: '!text-sm'
-}
-
-export default function Heading ({ as, children, ...rest }: HeadingProps): React.ReactElement {
+export default function Heading ({ as, leadingIcon, trailingIcon, children, ...rest }: HeadingProps): React.ReactElement {
   const Component = as ?? 'h1'
 
+  const className = `
+    inline-flex items-center gap-2 text-xl font-semibold tracking-wide
+    ${HEADING_STYLES[as ?? 'h1']}
+    ${rest.className as string}
+  `
+
+  delete rest.className
+
   return (
-    <Component
-      className={ `text-xl font-semibold tracking-wide ${HEADING_STYLES[Component]}` }
-      {...rest}
-    >
-      {children}
+    <Component className={className} {...rest}>
+      { leadingIcon != null ? <Icon icon={leadingIcon} size={ICON_SIZES[Component] as IconSize}/> : null }
+      <div>{children}</div>
+      { trailingIcon != null ? <Icon icon={trailingIcon} size={ICON_SIZES[Component] as IconSize}/> : null }
     </Component>
   )
 }

840f27adfd4fc8ade3eb0335b2dd9b20c3006802
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-05-08 09:02:37 +0200

    feat: add new transparent variant for button and icon buttons.

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -1,7 +1,11 @@
+export type ButtonVariant = keyof typeof BUTTON_VARIANTS
+
+export type ButtonSize = keyof typeof BUTTON_SIZES
+
 interface ButtonProps {
   as?: 'button' | 'a' | 'div'
-  variant?: 'primary'
-  size?: 'sm' | 'base'
+  variant?: ButtonVariant
+  size?: ButtonSize
   children?: React.ReactNode
   [x: string]: unknown
 }
@@ -11,12 +15,18 @@ export const BUTTON_VARIANTS = {
     border border-gray-700 text-gray-700 font-semibold
     hover:bg-gray-700 hover:text-white
     disabled:bg-gray-300 disabled:text-gray-300
+  `,
+  transparent: `
+    text-gray-700 font-semibold
+    hover:bg-gray-700 hover:text-white
+    disabled:bg-gray-300 disabled:text-gray-300
   `
 }
 
 export const BUTTON_SIZES = {
   sm: 'text-[10px] h-[24px] px-6',
-  base: 'text-sm h-[34px] px-10'
+  base: 'text-sm h-[34px] px-10',
+  lg: 'text-lg h-[38px] px-12'
 }
 
 export default function Button ({ as, variant, size, children, ...rest }: ButtonProps): React.ReactElement {

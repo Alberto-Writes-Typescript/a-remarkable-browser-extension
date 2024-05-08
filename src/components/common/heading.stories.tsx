a343c2d4a416db22e18cf981d37d6f11412a6980
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-05-08 09:29:31 +0200

    feat: Add leading and trailing icons for heading component.
    I also did some clean up of the code, updating the stories and fixing the load of inlined classes.

--- src/components/common/heading.stories.tsx
+++ src/components/common/heading.stories.tsx
@@ -1,13 +1,36 @@
-import heading from './heading'
+import { type StoryFn } from '@storybook/react'
+
+import Heading, { type HeadingProps, HEADING_STYLES } from './heading'
+import { ICONS_LIBRARY } from './icon'
 
 export default {
   title: 'Common/Heading',
-  component: heading
-}
-
-export const Default = {
-  args: {
-    as: 'h1',
-    children: 'Heading Component'
+  component: Heading,
+  argTypes: {
+    as: {
+      control: 'select',
+      options: Object.keys(HEADING_STYLES)
+    },
+    leadingIcon: {
+      control: 'select',
+      options: Object.keys(ICONS_LIBRARY)
+    },
+    trailingIcon: {
+      control: 'select',
+      options: Object.keys(ICONS_LIBRARY)
+    }
   }
 }
+
+const Template: StoryFn<HeadingProps> = (args) => <Heading {...args} >Heading</Heading>
+
+export const Playground = Template.bind({})
+
+export const H1 = Template.bind({})
+H1.args = { as: 'h1' }
+
+export const H2 = Template.bind({})
+H2.args = { as: 'h2' }
+
+export const H3 = Template.bind({})
+H3.args = { as: 'h3' }
